225758e476efa701293be54c4ac8818b584d2306
SteWil <32218463+Stewil@users.noreply.github.com>
2023-01-22 19:23:42 +0100

    fixed more jammy naming

--- ubuntu-server/04-software.sh
+++ ubuntu-server/04-software.sh
@@ -3,7 +3,7 @@ ROOTDIR=$(dirname $(realpath "$0"))
 CFGDIR="${ROOTDIR}/config/"
 
 install_software(){
-    sudo apt install -y neofetch zip unzip unrar tar 7z gzip clang glibc bash-completion rxvt-unicode
+    sudo apt install -y neofetch zip unzip unrar tar 7zip gzip clang bash-completion rxvt-unicode
 }
 
 install_software
