4ad16ca7865f7f25356bdafb167ac15965ca6c49
SteWil <32218463+Stewil@users.noreply.github.com>
2023-01-31 14:02:11 +0100

    prints

--- arch-i3/00-prep.sh
+++ arch-i3/00-prep.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 ROOTDIR=$(dirname $(realpath "$0"))
 source $ROOTDIR/arch-utils.sh
+echo "ROOT: $ROOTDIR"
 
 install_base(){
     echo "INSTALL BASE"
--- arch-i3/01-network.sh
+++ arch-i3/01-network.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 ROOTDIR=$(readlink -f “${BASH_SOURCE:-$0}”)
 source $ROOTDIR/arch-utils.sh
+echo "ROOT: $ROOTDIR"
 
 install_netman(){
     echo "INSTALL NETWORK MANAGER"
