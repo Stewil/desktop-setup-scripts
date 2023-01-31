e6a1a5838bde1bf786f5bc4f2c1d4b455b1c0e06
SteWil <32218463+Stewil@users.noreply.github.com>
2023-01-31 15:31:19 +0100

    set nvim as default

--- config-user.sh
+++ config-user.sh
@@ -28,6 +28,9 @@ config_neovim(){
     echo "CONFIGURING NEOVIM"
     curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 	cp -r "$CFGDIR"/nvim ~/.config/
+    sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 100
+    sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 100
+    sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 100
 }
 
 config_aliases(){
