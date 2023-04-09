f57aebc8eb247bd51e84bebae3b013f2561f4d8f
Stefan Wilde <32218463+Stewil@users.noreply.github.com>
2023-04-09 12:31:09 +0200

    removed faulty function

--- ubuntu-i3/ubuntu-utils.sh
+++ ubuntu-i3/ubuntu-utils.sh
@@ -9,17 +9,3 @@ ADD(){
 REMOVE(){
     sudo DEBIAN_FRONTEND=noninteractive apt purge -y "$@"
 }
-
-EXISTS(){
-    # call as:
-    # EXISTS line='thisline' file='/home/file.txt'
-    local "${@}"
-    if [ -z ${file+x} ]; then 
-        echo "file to check was not given" && return -1
-    fi
-    if [ -z ${line+x} ]; then 
-        echo "line to check was not given" && return -1
-    fi
-    return grep -Fxq "$line" "$file"
-}
-
