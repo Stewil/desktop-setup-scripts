b5ebaeb97e925a02f6773e4386822edca29948d0
Stefan Wilde <32218463+Stewil@users.noreply.github.com>
2023-04-20 14:08:18 +0200

    suppress stdout attempt

--- ubuntu-i3/02-ui.sh
+++ ubuntu-i3/02-ui.sh
@@ -9,42 +9,42 @@ install_drivers(){
 
 install_wm(){
     echo "INSTALLING WINDOW MANAGER"
-    ADD xorg i3-wm picom i3blocks i3lock i3status dunst rofi nitrogen arandr xinit scrot lightdm lightdm-gtk-greeter gnome-themes-extra
-    ADD python-is-python3 python3-pip
-    ADD libxcb-render0-dev libffi-dev python3-dev python3-cffi
-    ADD dbus-x11 at-spi2-core
-    python3 -m pip install flashfocus
-    sudo dpkg-reconfigure lightdm
+    ADD xorg i3-wm picom i3blocks i3lock i3status dunst rofi nitrogen arandr xinit scrot lightdm lightdm-gtk-greeter gnome-themes-extra > /dev/null
+    ADD python-is-python3 python3-pip > /dev/null
+    ADD libxcb-render0-dev libffi-dev python3-dev python3-cffi > /dev/null
+    ADD dbus-x11 at-spi2-core > /dev/null
+    python3 -m pip install flashfocus > /dev/null
+    sudo dpkg-reconfigure lightdm > /dev/null
 }
 
 install_tools(){
     echo "INSTALLING VARIOUS TOOLS"
-    ADD policykit-1-gnome thunar flameshot x11-xkb-utils mpv powerline libimlib2
+    ADD policykit-1-gnome thunar flameshot x11-xkb-utils mpv powerline libimlib2 > /dev/null
     echo "INSTALLING FONTS"
-    ADD fonts-noto* 
+    ADD fonts-noto*  > /dev/null
     if [ ! -f "$HOME/.local/share/fonts/DejaVu Sans Mono for Powerline.ttf" ]; then
     mkdir -p $HOME/.local/share/fonts
         sudo bash -c "git clone https://github.com/powerline/fonts.git \
-            --depth=1 /tmp/fonts && cd /tmp/fonts && ./install.sh"
+            --depth=1 /tmp/fonts > /dev/null && cd /tmp/fonts && ./install.sh > /dev/null"
     fi
     if [ ! -f "$HOME/.local/share/fonts/Font Awesome 6 Free-Regular-400.otf" ]; then
-        git clone https://github.com/FortAwesome/Font-Awesome /tmp/fa && \
-            cp /tmp/fa/otfs/* $HOME/.local/share/fonts/
+        git clone https://github.com/FortAwesome/Font-Awesome /tmp/fa > /dev/null && \
+            cp /tmp/fa/otfs/* "$HOME/.local/share/fonts/" > /dev/null
     fi
     if [ ! -f /usr/local/bin/nsxiv ]; then
         echo "BUILDING AND INSTALLING NSXIV"
-        sudo bash -c "git clone https://codeberg.org/nsxiv/nsxiv /tmp/nsxiv && \
-            cd /tmp/nsxiv && make && make install"
+        sudo bash -c "git clone https://codeberg.org/nsxiv/nsxiv /tmp/nsxiv  > /dev/null && \
+            cd /tmp/nsxiv && make > /dev/null && make install > /dev/null"
     fi
-    fc-cache -f -v
+    fc-cache -f -v  > /dev/null
 }
 
 install_applications(){
-    echo "INSTALLING APPLICATIONS"
-    ADD firefox
+    echo "INSTALLING APPLICATIONS" 
+    ADD firefox > /dev/null
     if [ ! -f /usr/bin/nvim ]; then
-        wget -P /tmp https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
-        sudo bash -c "cd /tmp && tar -xf nvim-linux64.tar.gz && cp -r nvim-linux64/* /usr/local/ || echo 'INSTALLING NEOVIM FAILED'"
+        wget -P /tmp https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz > /dev/null
+        sudo bash -c "cd /tmp && tar -xf nvim-linux64.tar.gz > /dev/null && cp -r nvim-linux64/* /usr/local/  > /dev/null|| echo 'INSTALLING NEOVIM FAILED'"
     fi
 }
 
