6c42f39d9cd749c66d39cfab67fbad840e30067d
SteWil <32218463+Stewil@users.noreply.github.com>
2023-09-11 15:22:14 +0200

    added python3 neovim

--- arch-i3/04-software.sh
+++ arch-i3/04-software.sh
@@ -4,7 +4,7 @@ source $ROOTDIR/arch-utils.sh
 
 install_software(){
     echo "INSTALLING COMMONLY USED SOFTWARE"
-    ADD rxvt-unicode curl wget neofetch neovim unrar zip unzip htop clang cmake gcc glibc gimp bash-completion xclip imagemagick mpv fuse2 keepassxc libwebp imlib2 sysstat
+    ADD rxvt-unicode curl wget neofetch neovim unrar zip unzip htop clang cmake gcc glibc gimp bash-completion xclip imagemagick mpv fuse2 keepassxc libwebp imlib2 sysstat python-neovim python-pynvim
 }
 
 install_software
